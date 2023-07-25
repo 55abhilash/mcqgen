565f914ec1e35a627b65823c1ca869e2f515030c
Abhilash <abhilashmhaisne@gmail.com>
2023-07-26 05:18:10 +0530

    Minor fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mcqgen',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Abhilash Mhaisne',
     description='Generate Multiple Choice Questions, Choices and Correct Answer in JSON Format',
