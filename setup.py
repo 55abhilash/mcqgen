2b9a83c69d984d74a0038b92fa5902b76eb3140c
Abhilash <abhilashmhaisne@gmail.com>
2023-07-26 05:30:38 +0530

    Add usage steps to README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mcqgen',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author='Abhilash Mhaisne',
     description='Generate Multiple Choice Questions, Choices and Correct Answer in JSON Format',
